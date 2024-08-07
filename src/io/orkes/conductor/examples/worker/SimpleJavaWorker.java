3fac39ca691979eb7b00dd257d6b32ae73241be0
Miguel Prieto <miguel.prieto@orkes.io>
2024-08-06 22:34:18 -0400

    Replaced log4j direct usage with slf4j

--- src/io/orkes/conductor/examples/worker/SimpleJavaWorker.java
+++ src/io/orkes/conductor/examples/worker/SimpleJavaWorker.java
@@ -7,15 +7,14 @@ import io.orkes.conductor.client.ApiClient;
 import io.orkes.conductor.client.OrkesClients;
 import io.orkes.conductor.client.TaskClient;
 import io.orkes.conductor.client.automator.TaskRunnerConfigurer;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class SimpleJavaWorker implements Worker {
 
-    private static final Logger LOGGER = LogManager.getLogger(SimpleJavaWorker.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(SimpleJavaWorker.class);
 
     // Get your KEY and SECRET from your server - https://www.youtube.com/watch?v=f1b5vZRKn2Q
     private static final String ROOT_URI = "http://localhost:8080/api";
